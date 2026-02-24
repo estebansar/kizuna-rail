e333c1f86bb9c23875d255d024749c33b9e9e868
estebansar <sar17008@byui.edu>
2026-02-23 22:52:33 -0700

    Task 3 -  filtering for region and season

--- src/routes/routes/list.js
+++ src/routes/routes/list.js
@@ -1,8 +1,17 @@
 import { getAllRoutes, getListOfRegions, getListOfSeasons } from '../../models/model.js';
 
 export default async (req, res) => {
+    const { region, season } = req.query; /**added as part of checklist_3 */
+
     const regions = await getListOfRegions();
-    const routes = await getAllRoutes();
+    let routes = await getAllRoutes(); /**added as part of checklist_3 */
+    if (region) {
+        routes = routes.filter(route => route.region === region);
+    }
+
+    if (season) {
+        routes = routes.filter(route => route.bestSeason === season);
+    }
     const seasons = await getListOfSeasons();
 
     res.render('routes/list', { 
