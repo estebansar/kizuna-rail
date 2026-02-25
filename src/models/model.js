4dd117415705db398ceeec69ef3e701a15175603
estebansar <sar17008@byui.edu>
2026-02-24 21:55:25 -0700

    Task 6 - Add print styles for confirmation page

--- src/models/model.js
+++ src/models/model.js
@@ -1,4 +1,4 @@
-import { generateConfirmationCode, monthNumberToAbbreviation } from '../includes/helpers.js';
+import { generateConfirmationCode, monthNumberToAbbreviation, yenToUsd } from '../includes/helpers.js';
 import { getDb as db } from './db-in-file.js';
 
 // ROUTE MODEL FUNCTIONS
@@ -175,7 +175,7 @@ export const getTicketOptionsForRoute = async (routeId) => {
     return db().ticketClasses.map(tc => ({
         class: tc.class,
         name: tc.name,
-        price: route.distance * tc.pricePerKm,
+        price: Math.round(yenToUsd(route.distance * tc.pricePerKm) * 100) / 100,
         amenities: tc.amenities,
         description: tc.description
     }));
